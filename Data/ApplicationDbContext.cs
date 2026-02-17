84ec493978a90845bcef1690ffd85944664020b4
tarydenise <tarydenise@gmail.com>
2026-02-17 05:36:54 -0800

    Add Recipe model, DbContext setup, and initial migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PantryPilot.Models;
+
+namespace PantryPilot.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,10 @@
+namespace PantryPilot.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }  
+        public string Name { get; set; }  
+        public string Ingredients { get; set; }  
+        public string Instructions { get; set; }  
+    }
+}
