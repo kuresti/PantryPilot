ce9389fdfb6c98a96ad47afe3890e2ea2216ceda
tarydenise <tarydenise@gmail.com>
2026-02-20 00:27:58 -0800

    Add models, relationships, and migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,6 +10,14 @@ namespace PantryPilot.Data
         {
         }
 
+        public DbSet<User> Users { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<RecipeStep> RecipeSteps { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuRecipe> MenuRecipes { get; set; }
+        public DbSet<GroceryList> GroceryLists { get; set; }
+        public DbSet<GroceryListItem> GroceryListItems { get; set; }
     }
 }
--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -4,7 +4,16 @@ namespace PantryPilot.Models
     {
         public int Id { get; set; }  
         public string Name { get; set; }  
-        public string Ingredients { get; set; }  
-        public string Instructions { get; set; }  
+        public string Description { get; set; }  
+        public int PrepTime { get; set; }  
+        public int CookTime { get; set; }  
+        public int Servings { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }   
+
+        public List<RecipeStep> Steps { get; set; }
+        public List<RecipeIngredient> RecipeIngredients { get; set; }
+        public List<MenuRecipe> MenuRecipes { get; set; }
     }
 }
