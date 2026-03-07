6c0098ac1b3260a08e653e5905979a25119a0c0a
kuresti <55415865+kuresti@users.noreply.github.com>
2026-03-07 10:13:49 -0700

    Update Program.cs
    * Services were added for Identity so that Authorization and Authentication functionality could be added to the app.
    * Middleware was also added for Authentication and Authorization functionality.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,36 @@
-using BlazorApp.Components;
+using PantryPilot.Components;
 using PantryPilot.Data;
 using Microsoft.EntityFrameworkCore;
 using PantryPilot.Models;
 using Microsoft.AspNetCore.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=pantry.db"));
 
 // This configures Identity for user authentication
-builder.Services.AddIdentity<ApplicationUser, IdentityRole<int>>()
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<int>>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 8;
+})
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
+
+// Adds authorization services
+builder.Services.AddAuthorization();
+
+// Adds authentication state
+builder.Services.AddCascadingAuthenticationState();
 
 var app = builder.Build();
 
@@ -28,6 +43,14 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
+
+// Middleware for Authentication
+// Processes login cookies
+app.UseAuthentication();
+
+// Enforces authorization
+app.UseAuthorization();
+
 
 app.UseAntiforgery();
 
