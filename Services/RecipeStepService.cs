c6b876b8fc7e49e1f474943cd3938292ff6305ea
tarydenise <tarydenise@gmail.com>
2026-03-19 00:13:58 -0700

    Add RecipeStepService with CRUD
    For managing recipe steps

---
+++ Services/RecipeStepService.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using PantryPilot.Data;
+using PantryPilot.Models;
+
+public class RecipeStepService
+{
+    private readonly ApplicationDbContext _context;
+
+    public RecipeStepService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<RecipeStep>> GetByRecipeIdAsync(int recipeId)
+    {
+        return await _context.RecipeSteps
+            .Where(rs => rs.RecipeId == recipeId)
+            .OrderBy(rs => rs.StepNumber)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(RecipeStep recipeStep)
+    {
+        _context.RecipeSteps.Add(recipeStep);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(RecipeStep recipeStep)
+    {
+        _context.RecipeSteps.Update(recipeStep);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task DeleteAsync(int id)
+    {
+        var recipeStep = await _context.RecipeSteps.FindAsync(id);
+        if (recipeStep ! null)
+        {
+            _context.RecipeSteps.Remove(recipeStep);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
