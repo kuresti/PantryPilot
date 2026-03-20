9a18b6ca5d8f0ac9cc6de706addba9c20a832423
kuresti <55415865+kuresti@users.noreply.github.com>
2026-03-20 14:29:12 -0600

    Create IWeeklyMenuService.cs
    Created IWeeklyMenuService interface

---
+++ Services/Interfaces/IWeeklyMenuService.cs
@@ -0,0 +1,12 @@
+using PantryPilot.Models;
+
+namespace PantryPilot.Services.Interfaces;
+
+public interface IWeeklyMenuService
+{
+    Task<WeeklyMenu> CreateWeeklyMenuAsync(WeeklyMenu weeklyMenu);
+    Task<List<WeeklyMenu>> GetAllWeeklyMenusAsync();
+    Task<WeeklyMenu?> GetWeeklyMenuByIdAsync(int id);
+    Task<bool> UpdateWeeklyMenuAsync(WeeklyMenu weeklyMenu);
+    Task<bool> DeleteWeeklyMenuAsync(int id);
+}
