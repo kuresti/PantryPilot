c0a412b41b444691d4db2cb5535cde980e8b2342
kuresti <55415865+kuresti@users.noreply.github.com>
2026-03-20 14:28:03 -0600

    Create IMenuRecipeService.cs
    Created IMenuRecipeService interface

---
+++ Services/Interfaces/IMenuRecipeService.cs
@@ -0,0 +1,11 @@
+using PantryPilot.Models;
+
+namespace PantryPilot.Services.Interfaces;
+
+public interface IMenuRecipeService
+{
+    Task<MenuRecipe?> AddRecipeToMenuAsync(int menuId, int recipeId);
+    Task<List<MenuRecipe>> GetRecipesForMenuASync(int menuId);
+    Task<bool> RemoveRecipeFromMenuAsync(int menuRecipeId);
+
+}
