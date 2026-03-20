3091c63441026fb6571c4c41df1007b35dd49875
kuresti <55415865+kuresti@users.noreply.github.com>
2026-03-20 14:26:17 -0600

    Update Program.cs
    Registered all created interfaces:
    -IRecipeIngredientService
    -IIngredientService
    -IMenuService
    -IMenuRecipeService
    -IMenuDayService
    -IWeeklyMenuService
    -IRecipeService

--- Program.cs
+++ Program.cs
@@ -18,14 +18,13 @@ builder.Services.AddRazorComponents()
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("ApplicationDbContext")));
 
-builder.Services.AddScoped<RecipeService>();
-builder.Services.AddScoped<RecipeStepService>();
-builder.Services.AddScoped<RecipeIngredientService>();
-builder.Services.AddScoped<IngredientService>();
-builder.Services.AddScoped<MenuService>();
-builder.Services.AddScoped<MenuRecipeService>();
-builder.Services.AddScoped<MenuDayService>();
-builder.Services.AddScoped<WeeklyMenuService>();
+builder.Services.AddScoped<IRecipeStepService, RecipeStepService>();
+builder.Services.AddScoped<IRecipeIngredientService, RecipeIngredientService>();
+builder.Services.AddScoped<IIngredientService, IngredientService>();
+builder.Services.AddScoped<IMenuService,MenuService>();
+builder.Services.AddScoped<IMenuRecipeService, MenuRecipeService>();
+builder.Services.AddScoped<IMenuDayService, MenuDayService>();
+builder.Services.AddScoped<IWeeklyMenuService, WeeklyMenuService>();
 builder.Services.AddScoped<IRecipeService, RecipeService>();
 
 // Adds authentication state
