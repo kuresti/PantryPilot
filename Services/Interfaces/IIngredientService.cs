a9673400177dd0260df550e0beb30f107f950516
kuresti <55415865+kuresti@users.noreply.github.com>
2026-03-20 14:27:19 -0600

    Create IIngredientService.cs
    Created IIngredientService interface

---
+++ Services/Interfaces/IIngredientService.cs
@@ -0,0 +1,12 @@
+using PantryPilot.Models;
+
+namespace PantryPilot.Services.Interfaces;
+
+public interface IIngredientService
+{
+    Task<List<Ingredient>> GetAllAsync();
+    Task<Ingredient?> GetByIdAsync(int id);
+    Task AddAsync(Ingredient ingredient);
+    Task UpdateAsync(Ingredient ingredient);
+    Task DeleteAsync(int id);
+}
