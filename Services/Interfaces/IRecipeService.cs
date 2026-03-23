9253e849b619b4753189f38e647fedd810b190d4
kuresti <55415865+kuresti@users.noreply.github.com>
2026-03-23 15:32:50 -0600

    Create IRecipeService.cs
    - Created IRecipeService interface so that app can depend on behavior without exposing implementation.

---
+++ Services/Interfaces/IRecipeService.cs
@@ -0,0 +1,13 @@
+using PantryPilot.Models;
+
+namespace PantryPilot.Services.Interfaces;
+
+public interface IRecipeService
+{
+    Task<int> GetCurrentUserId();
+    Task<List<Recipe>> GetUserRecipes();
+    Task<Recipe?> GetRecipe(int id);
+    Task<Recipe> CreateRecipe(Recipe recipe);
+    Task<bool> UpdateRecipe(Recipe recipe);
+    Task<bool> DeleteRecipe(int id);
+}
