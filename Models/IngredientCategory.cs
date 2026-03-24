c11253fa6edd52b0a202fea9a313b88fe77dd64c
kuresti <55415865+kuresti@users.noreply.github.com>
2026-03-23 20:19:41 -0600

    Create IngredientCategory.cs
    - Created IngredientCategory enum

---
+++ Models/IngredientCategory.cs
@@ -0,0 +1,16 @@
+// Enumerator to define Grocery IngredientCategories
+namespace PantryPilot.Models;
+
+public enum IngredientCategory
+{
+    Produce,
+    Dairy,
+    Meat,
+    Frozen,
+    Deli,
+    Packaged,
+    Canned,
+    Bakery,
+    Beverage,
+    Other
+}
